2188b347d5bcd2d448cfd136e8b88b033fe14c11
Galchinsky Vadim <rorshah.009@gmail.com>
2018-06-12 22:31:59 +0300

    page login - validation [6]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -14,7 +13,6 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AuthModule,
-    FormsModule,
     HttpModule,
     AppRoutingModule
   ],
