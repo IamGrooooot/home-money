04fba4aee67e7f5b0d79a5f18ce70d336b925e3c
Galchinsky Vadim <rorshah.009@gmail.com>
2018-06-13 23:28:23 +0300

    create module system [11]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersService } from './shared/services/users.service';
 import { AuthService } from './shared/services/auth.service';
+import { SystemModule } from './system/system.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AuthService } from './shared/services/auth.service';
     AuthModule,
     HttpModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SystemModule
   ],
   providers: [
     UsersService,
