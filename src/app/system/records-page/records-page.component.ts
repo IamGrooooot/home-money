de2884fa7152dd91741c19c63a67bd5371b77a83
Galchinsky Vadim <rorshah.009@gmail.com>
2018-06-16 13:36:13 +0300

    page records, edit category [21]

--- src/app/system/records-page/records-page.component.ts
+++ src/app/system/records-page/records-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Category } from '../shared/models/category.model';
+import { CategoriesService } from '../shared/services/catigories.service';
 
 @Component({
   selector: 'wfm-records-page',
@@ -9,12 +10,28 @@ import { Category } from '../shared/models/category.model';
 })
 export class RecordsPageComponent implements OnInit {
 
-  constructor() { }
+  private categories: Category[] = [];
+  isLoaded: boolean = false;
+
+  constructor(
+    private categoryService: CategoriesService
+  ) { }
 
   ngOnInit() {
+    this.categoryService.getCatigories()
+      .subscribe((categories: Category[]) => {
+        this.categories = categories;
+        this.isLoaded = true;
+      });
   }
 
   newCategoryAdded(category: Category){
-    //add to array
+    this.categories.push(category);
+  }
+
+  categoryWasEdited(category: Category){
+    const idx = this.categories
+      .findIndex(c => c.id === category.id);
+    this.categories[idx] = category;
   }
 }
