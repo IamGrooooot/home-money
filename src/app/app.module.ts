1dd395a365c80b5f046d439005becf976ce9c3b4
Galchinsky Vadim <rorshah.009@gmail.com>
2018-06-18 22:49:23 +0300

    page history, schedule [26]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
@@ -20,7 +21,8 @@ import { SystemModule } from './system/system.module';
     HttpModule,
     AppRoutingModule,
     HttpClientModule,
-    SystemModule
+    SystemModule,
+    BrowserAnimationsModule
   ],
   providers: [
     UsersService,
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CategoriesService } from '../shared/services/catigories.service';
+import { EventsService } from '../shared/services/events.service';
+import { Observable, combineLatest, Subscription } from 'rxjs';
+import { Category } from '../shared/models/category.model';
+import { WFMEvent } from '../shared/models/event.model';
 
 @Component({
   selector: 'wfm-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.scss']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  sub1: Subscription;
 
+  constructor(
+    private catigoriesService: CategoriesService,
+    private eventsServise: EventsService
+  ) { }
+
+  isLoaded = false;
+  categories: Category[] = [];
+  events: WFMEvent[] = [];
+  chartData = [];
   ngOnInit() {
+    this.sub1 = Observable.call(
+      combineLatest(
+        this.catigoriesService.getCatigories(),
+        this.eventsServise.getEvents()
+      )
+      .subscribe((data: [ Category[], WFMEvent[]]) => {
+        this.categories = data[0];
+        this.events = data[1];
+
+        this.calculateChartData();
+        this.isLoaded = true;
+      })
+    );
   }
 
+  calculateChartData() : void{
+    this.chartData = [];
+    this.categories.forEach((cat) => {
+      const catEvents = this.events.filter((e) => e.category === cat.id && e.type === "outcome");
+      this.chartData.push({
+        name: cat.name,
+        value: catEvents.reduce((total, e) => {
+          total += e.amount;
+          return total;
+        }, 0)
+      })
+    });
+  }
+
+  ngOnDestroy(){
+    if(this.sub1){
+      this.sub1.unsubscribe();
+    }
+  }
 }
